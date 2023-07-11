4a471c3b594d3baa6254abc87e89294a252194fe
AMIT KUMAR <88123144+amit-kumar81@users.noreply.github.com>
2023-07-11 23:07:09 +0530

    jsdhs.js

--- visualize_sorting_algo_with _React/src/components/Footer.js
+++ visualize_sorting_algo_with _React/src/components/Footer.js
@@ -3,7 +3,7 @@ function Footer() {
     return (
         <>
             <div className='bottom-0 w-full bg-[#222831]'>
-                <h1 className='text-[#00ADB5] text-xl text-center pt-4'>All credits reserved to @krcpr007</h1>
+                <h1 className='text-[#00ADB5] text-xl text-center pt-4'>All credits reserved to amit-kumar81r</h1>
             </div>
         </>
     )
